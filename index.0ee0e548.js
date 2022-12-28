a9ad9a33735e46933aba8de1e293e0acacb42d60
Volodymyr-Mospan <109673202+Volodymyr-Mospan@users.noreply.github.com>
2022-12-28 22:17:52 +0000

    Deploying to gh-pages from @ Volodymyr-Mospan/goit-js-hw-10@f3176eae18263c4d81e2fa492997265638d8b865 🚀

--- index.0ee0e548.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#search-box");console.log(e);e.addEventListener("input",(function(o){console.log(e.value)}));
-//# sourceMappingURL=index.0ee0e548.js.map
--- index.d087386e.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#search-box");console.log(e);e.addEventListener("input",(function(o){console.log(e.value)}))}();
-//# sourceMappingURL=index.d087386e.js.map
